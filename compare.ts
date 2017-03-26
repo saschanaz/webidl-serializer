c6693b0ff2c75b1db0b54ce9bbabf24e4df2f9c2
Kagami Sascha Rosylight <saschanaz@outlook.com>
2017-03-27 02:36:09 +0900

    ignore deprecated interfaces

--- compare.ts
+++ compare.ts
@@ -8,22 +8,37 @@ run();
 async function run() {
     const msedgeDocument = new DOMParser().parseFromString(await fspromise.readFile("supplements/browser.webidl.xml"), "text/xml");
     const standardDocument = new DOMParser().parseFromString(await fspromise.readFile("built/all.webidl.xml"), "text/xml");
+    const ignore = JSON.parse(await fspromise.readFile("specs-ignore.json"));
 
-    compareArray(extractInterfaceNames(msedgeDocument), extractInterfaceNames(standardDocument));
+    compareArray(extractInterfaceNames(msedgeDocument), extractInterfaceNames(standardDocument), ignore);
 }
 
 function extractInterfaceNames(doc: Document) {
+    const callbackInterfaces = doc.getElementsByTagName("callback-interfaces")[0];
     const interfaces = doc.getElementsByTagName("interfaces")[0];
     const mixinInterfaces = doc.getElementsByTagName("mixin-interfaces")[0];
 
-    return [...getChildrenArray(interfaces), ...getChildrenArray(mixinInterfaces)].map(interfaceEl => interfaceEl.getAttribute("name"));
+    return [
+        ...getChildrenArray(callbackInterfaces),
+        ...getChildrenArray(interfaces),
+        ...getChildrenArray(mixinInterfaces)
+    ].filter(interfaceEl => interfaceEl.getAttribute("tags") !== "MSAppOnly").map(interfaceEl => interfaceEl.getAttribute("name"));
 }
 
-function compareArray(base: string[], comparand: string[]) {
+function compareArray(base: string[], comparand: string[], ignore: string[]) {
+    for (const item of ignore) {
+        if (base.indexOf(item) === -1) {
+            console.log(`${item} is removed in base xml so no need to check anymore.`);
+        }
+    }
     // naive algorithm
     let count = 0;
     for (const item of base) {
         if (comparand.indexOf(item) === -1) {
+            if (ignore.indexOf(item) !== -1) {
+                // ignore
+                continue;
+            }
             count++;
             console.warn(`${item} is not found in comparand.`)
         }
