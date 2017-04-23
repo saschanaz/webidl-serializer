83d392aecbf6eba1231fe00cbbc6da5c05d15f4e
Kagami Sascha Rosylight <saschanaz@outlook.com>
2017-04-24 00:39:24 +0900

    strictEqual

--- test-sources/test.ts
+++ test-sources/test.ts
@@ -3,6 +3,6 @@ import * as chai from "chai";
 
 describe("Baseline comparison", () => {
     it("should same", async () => {
-        chai.assert.deepEqual(await mz.readFile("built/browser.webidl.xml", "utf8"), await mz.readFile("baseline/browser.webidl.xml", "utf8"));
+        chai.assert.strictEqual(await mz.readFile("built/browser.webidl.xml", "utf8"), await mz.readFile("baseline/browser.webidl.xml", "utf8"));
     })
 })
