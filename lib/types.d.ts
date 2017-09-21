83cca9a412a01b99015bc44b86ea1859d0d80afc
Kagami Sascha Rosylight <saschanaz@outlook.com>
2017-09-21 17:17:37 +0900

    json copy format for ms webidl-xml

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -32,3 +32,103 @@ export interface MSEdgeIgnore {
     events: string[],
     cssProperties: string[]
 }
+
+export namespace Definitions {
+    export interface CallbackFunction {
+        name: string;
+        callback: true,
+        nullable: boolean;
+        type: string;
+    }
+
+    export interface Dictionary {
+        name: string;
+        extends: string;
+        partial: boolean;
+        members: DictionaryMember[];
+    }
+
+    export interface DictionaryMember {
+        name: string;
+        default: string;
+        nullable: boolean;
+        type: string;
+        required: boolean;
+    }
+
+    export interface Enum {
+        name: string;
+        values: string[];
+    }
+
+    export interface Interface {
+        name: string;
+        extends: string;
+        partial?: boolean;
+
+        noInterfaceObject?: boolean;
+        namedConstructor?: { name: string; params: Param[]; };
+        constructor?: { params: Param[]; }[];
+        global?: string;
+        primaryGlobal?: string;
+        overrideBuiltins?: boolean;
+        exposed?: string[];
+
+        constants?: { name: string; nullable: boolean; value: string; }[];
+        operations?: Operation[];
+        attributes?: Attribute[];
+
+        iterable?: { keytype?: string; type: string; }
+    }
+
+    export interface namespace {
+        name: string;
+        partial?: boolean;
+
+        exposed?: string[];
+
+        operations?: Operation[];
+        attributes?: Attribute[];
+    }
+
+    export interface Operation {
+        name: string;
+        params: Param[]
+        nullable: boolean;
+        type: string;
+
+        getter?: true;
+        setter?: true;
+        creater?: true;
+        deleter?: true;
+        static?: true;
+        stringifier?: true;
+
+        exposed?: string[];
+    }
+
+    export interface Attribute {
+        name: string;
+        readOnly?: boolean;
+        static?: boolean;
+        stringifier?: boolean;
+        nullable?: boolean;
+        type: string;
+        exposed?: string[];
+    }
+
+    export interface Param {
+        name: string;
+        default: boolean;
+        optional: boolean;
+        nullable: boolean;
+        type: string;
+        variadic: boolean;
+    }
+
+    export interface Typedef {
+        name: string;
+        nullable: boolean;
+        type: string;
+    }
+}
